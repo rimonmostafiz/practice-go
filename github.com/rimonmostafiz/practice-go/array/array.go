c5a209a48187ee8b9b21005e169cab283bb38fbe
Rimon Mostafiz <rimonmostafiz@gmail.com>
2020-06-28 23:14:31 +0600

    Practice array creation basics

---
+++ github.com/rimonmostafiz/practice-go/array/array.go
@@ -0,0 +1,37 @@
+package array
+
+import (
+	"fmt"
+)
+
+func CreateArrays() {
+	grades := [5]int{87, 68, 97, 92, 83}
+	fmt.Printf("Array %v", grades)
+
+	numbers := [...]int{1, 2, 3}
+	for i := 0; i < len(numbers); i++ {
+		fmt.Printf("Index: %v, Value: %v", i, numbers[i])
+	}
+
+	gpa := []float32{4.00, 3.50, 5.00, 5.00, 4.00}
+	for _, val := range gpa {
+		fmt.Println(val)
+	}
+
+	var x [3]int
+	fmt.Println(x)
+	x[1] = 100
+	x[2] = 200
+	fmt.Println(x)
+
+	var identityMatrix [3][3]int = [3][3]int {
+		[3]int{1, 0, 0},
+		[3]int{0, 1, 0},
+		[3]int{0, 0, 1}}
+	for _, val := range identityMatrix {
+		for _, item := range val {
+			fmt.Printf("%v ", item)
+		}
+		fmt.Println()
+	}
+}
---
+++ github.com/rimonmostafiz/practice-go/main.go
@@ -0,0 +1,7 @@
+package main
+
+import ("./array")
+
+func main() {
+	array.CreateArrays()
+}
