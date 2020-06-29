d281e0d3f028e774831eaee403a3cc721d4badde
Rimon Mostafiz <rimonmostafiz@gmail.com>
2020-06-29 10:08:37 +0600

    Practice some more array examples
    - Declare multi dimensional array
    - Learned about value type and array copying

--- practice-go/array/array.go
+++ practice-go/array/array.go
@@ -24,7 +24,7 @@ func CreateArrays() {
 	x[2] = 200
 	fmt.Println(x)
 
-	var identityMatrix [3][3]int = [3][3]int {
+	var identityMatrix [3][3]int = [3][3]int{
 		[3]int{1, 0, 0},
 		[3]int{0, 1, 0},
 		[3]int{0, 0, 1}}
@@ -34,4 +34,21 @@ func CreateArrays() {
 		}
 		fmt.Println()
 	}
+
+	var matrix [3][3]int
+	matrix[0] = [3]int{1, 2, 3}
+	matrix[0] = [3]int{4, 5, 6}
+	matrix[0] = [3]int{7, 8, 9}
+	fmt.Println(matrix)
+
+	a := [...]int{1, 2, 5}
+	b := a
+	b[1] = 3
+	fmt.Println(a)
+	fmt.Println(b)
+
+	c := &a
+	c[1] = 10
+	fmt.Println(a)
+	fmt.Println(c)
 }
